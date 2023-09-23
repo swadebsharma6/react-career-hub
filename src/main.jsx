a7cb0e2c40bb20bd615a25fb5c3eee41e5b08969
swadebsharma6 <swadebriva2012@gmail.com>
2023-09-23 07:39:07 +0600

    primary setup of the project

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import {
+  RouterProvider,
+  createBrowserRouter
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Root from './components/Root/Root';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <RouterProvider router={router} />
+  </React.StrictMode>,
+)
